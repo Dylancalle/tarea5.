9b8dcf9bd6d1ab39e0cae4defdfcf11e1363786f
Dylan Calle <dylan.calle@ucb.edu.bo>
2024-08-20 08:15:56 -0400

    se puede saludar por idioma pero solo ingles y español

--- src/presenter.js
+++ src/presenter.js
@@ -3,6 +3,7 @@ import saludar from "./sumador";
 const nombre_in = document.querySelector("#nombre");
 const genero_in = document.querySelector("#genero");
 const edad_in = document.querySelector("#edad");
+const idioma_in = document.querySelector("#idioma");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -12,6 +13,7 @@ form.addEventListener("submit", (event) => {
    const nombre = nombre_in.value;
    const genero = genero_in.value;
    const edad = edad_in.value;
+   const idioma = idioma_in.value;
 
-   div.innerHTML = "<p>" + saludar(nombre, genero, edad) + "</p>";
+   div.innerHTML = "<p>" + saludar(nombre, genero, edad, idioma) + "</p>";
 });
--- src/sumador.js
+++ src/sumador.js
@@ -46,4 +46,42 @@ function saludar(nombre, genero, edad){
   
     return "Ingrese en genero F (femenino) o M (Masculino)"
 }
+
+function saludar(nombre, genero, edad, idioma){
+    if(nombre == "" || genero == "" || edad == "" || idioma == "") 
+    {
+        return "llene todos los espacios"
+    }
+    if(idioma=="español")
+    {
+    if(edad > 30){
+        if(genero == "F")
+        {
+            return "Hola Sra. " + nombre
+        }
+        if(genero == "M")
+        {   
+            return "Hola Sr. " + nombre
+        }     
+    }else{
+        return "Hola Joven " + nombre
+    }
+    return "Ingrese en genero F (femenino) o M (Masculino)"
+    }else if(idioma == "ingles"){
+    if(edad > 30){
+        if(genero == "F")
+        {
+            return "Hello Mrs. " + nombre
+        }
+        if(genero == "M")
+        {   
+            return "Hello Mr. " + nombre
+        }     
+    }else{
+        return "Hello Young " + nombre
+    }
+    return "Enter gender F (female) or M (male)"
+    }
+    
+}
 export default saludar;
