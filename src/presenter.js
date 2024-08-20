c60b28fad80c09c2ccbdb9e2370e833b9d99bd42
Dylan Calle <dylan.calle@ucb.edu.bo>
2024-08-20 08:03:54 -0400

    Se puede saludar por edad

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,7 @@ import saludar from "./sumador";
 
 const nombre_in = document.querySelector("#nombre");
 const genero_in = document.querySelector("#genero");
+const edad_in = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -10,6 +11,7 @@ form.addEventListener("submit", (event) => {
 
    const nombre = nombre_in.value;
    const genero = genero_in.value;
+   const edad = edad_in.value;
 
-   div.innerHTML = "<p>" + saludar(nombre, genero) + "</p>";
+   div.innerHTML = "<p>" + saludar(nombre, genero, edad) + "</p>";
 });
--- src/sumador.js
+++ src/sumador.js
@@ -25,4 +25,25 @@ function saludar(nombre, genero){
     }
     return "Ingrese en genero F (femenino) o M (Masculino)"
 }
+
+function saludar(nombre, genero, edad){
+    if(nombre == "" || genero == "" || edad == "") 
+    {
+        return "llene todos los espacios"
+    }
+    if(edad > 30){
+        if(genero == "F")
+        {
+            return "Hola Sra. " + nombre
+        }
+        if(genero == "M")
+        {   
+            return "Hola Sr. " + nombre
+        }     
+    }else{
+        return "Hola Joven " + nombre
+    }
+  
+    return "Ingrese en genero F (femenino) o M (Masculino)"
+}
 export default saludar;
