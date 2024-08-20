96658486a9a9dcf668e9d40be5ef4ce2a42a7eba
Dylan Calle <dylan.calle@ucb.edu.bo>
2024-08-20 07:55:50 -0400

    Se puede saludar por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,7 @@
-// import sumar from "./sumador";
+import saludar from "./sumador";
 
 const nombre_in = document.querySelector("#nombre");
-//const second = document.querySelector("#segundo-numero");
+const genero_in = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -9,7 +9,7 @@ form.addEventListener("submit", (event) => {
    event.preventDefault();
 
    const nombre = nombre_in.value;
-//   const secondNumber = Number.parseInt(second.value);
+   const genero = genero_in.value;
 
-   div.innerHTML = "<p>" + "Hola " + nombre + "</p>";
+   div.innerHTML = "<p>" + saludar(nombre, genero) + "</p>";
 });
--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,28 @@
-// function sumar(a, b) {
-//   return a + b;
-// }
+function saludar(nombre) {
+    if(nombre == "")
+    {
+        return "ingrese nombre"
+    }
+  return "hola" + nombre;
+}
 
-// export default sumar;
+function saludar(nombre, genero){
+    if(nombre == "")
+    {
+        return "ingrese nombre"
+    }
+    if(genero == "")
+    {
+        return "ingrese genero"
+    }
+    if(genero == "F")
+    {
+        return "Hola Sra. " + nombre
+    }
+    if(genero == "M")
+    {
+        return "Hola Sr. " + nombre
+    }
+    return "Ingrese en genero F (femenino) o M (Masculino)"
+}
+export default saludar;
